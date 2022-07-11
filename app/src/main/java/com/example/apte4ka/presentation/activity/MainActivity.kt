d06cf39b5eb3a918b2db581f347b2ea858bcbdc2
Tisbus <yog78@mail.ru>
2022-07-11 17:48:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/apte4ka/presentation/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.apte4ka.presentation.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.apte4ka.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
