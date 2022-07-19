c00c9a92af66563fc8aa601c1fca802cfde261d8
Tisbus <yog78@mail.ru>
2022-07-19 17:44:30 +0300

    start make AidKitDetailFragment and PreparationAddFragment

--- app/src/main/java/com/example/apte4ka/presentation/fragment/AidKitDetailFragment.kt
+++ app/src/main/java/com/example/apte4ka/presentation/fragment/AidKitDetailFragment.kt
@@ -5,7 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.apte4ka.R
 import com.example.apte4ka.databinding.FragmentAidKitDetailBinding
 
 class AidKitDetailFragment : Fragment() {
@@ -36,6 +39,15 @@ class AidKitDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.i("check", aidKitId.toString())
+        addPreparation()
+    }
+
+    private fun addPreparation() {
+        bind.linerAddPrepBlock.setOnClickListener {
+            val bundle = bundleOf(AID_KIT_ID to aidKitId)
+            findNavController().navigate(R.id.action_aidKitDetailFragment_to_preparationAddFragment,
+                bundle)
+        }
     }
 
     companion object{
--- app/src/main/java/com/example/apte4ka/presentation/fragment/PreparationAddFragment.kt
+++ app/src/main/java/com/example/apte4ka/presentation/fragment/PreparationAddFragment.kt
@@ -5,56 +5,64 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.apte4ka.R
+import com.example.apte4ka.databinding.FragmentPreparationAddBinding
+import com.example.apte4ka.presentation.adapter.listaidkit.ListAidKitAdapter
+import com.example.apte4ka.presentation.viewmodel.aidkit.AidKitViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PreparationAddFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PreparationAddFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var aidKitId : Int? = null
+
+    private var _bind : FragmentPreparationAddBinding? = null
+    private val bind : FragmentPreparationAddBinding
+    get() = _bind?: throw RuntimeException("FragmentPreparationAddBinding == null")
+
+    private lateinit var adapterListAidKit : ListAidKitAdapter
+
+    private lateinit var viewModel : AidKitViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        parseArgs()
         super.onCreate(savedInstanceState)
+    }
+
+    private fun parseArgs() {
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            aidKitId = it.getInt(AID_KIT_ID)
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preparation_add, container, false)
+    ): View {
+        _bind = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_preparation_add, container, false)
+        return bind.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PreparationAddFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PreparationAddFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[AidKitViewModel::class.java]
+        viewModel.listAidKit.observe(viewLifecycleOwner){
+            adapterListAidKit.submitList(it)
+        }
+        recyclerSetup()
+    }
+
+    private fun recyclerSetup() : RecyclerView {
+        val recyclerView = bind.recyclerListSetAidKit
+        with(recyclerView) {
+            adapterListAidKit = ListAidKitAdapter()
+            adapter = adapterListAidKit
+        }
+        return recyclerView
+    }
+
+    companion object{
+        const val AID_KIT_ID = "aid_id"
     }
 }
