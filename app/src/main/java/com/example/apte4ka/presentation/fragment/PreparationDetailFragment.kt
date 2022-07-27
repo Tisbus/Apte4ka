520f2bc4a0dc28598a387f3d537b19ed8d99cd84
Tisbus <yog78@mail.ru>
2022-07-27 14:29:44 +0300

    add prep_detail is finish and work

--- app/src/main/java/com/example/apte4ka/presentation/fragment/PreparationDetailFragment.kt
+++ app/src/main/java/com/example/apte4ka/presentation/fragment/PreparationDetailFragment.kt
@@ -5,56 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.apte4ka.R
+import com.example.apte4ka.databinding.FragmentPreparationDetailBinding
+import com.example.apte4ka.presentation.viewmodel.preparation.PreparationViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PreparationDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PreparationDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+    private var idPrep : Int? = null
+
+    private var _bind : FragmentPreparationDetailBinding? = null
+    private val bind : FragmentPreparationDetailBinding
+    get() = _bind ?: throw RuntimeException("FragmentPreparationDetailBinding == null")
+
+    private lateinit var viewModelPrep : PreparationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            idPrep = it.getInt(AID_KIT_ID)
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preparation_detail, container, false)
+    ): View {
+        _bind = FragmentPreparationDetailBinding.inflate(inflater, container, false)
+        return bind.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PreparationDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PreparationDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModelPrep = ViewModelProvider(this)[PreparationViewModel::class.java]
+        getData()
+    }
+
+    private fun getData() {
+        bind.detailPrep = viewModelPrep
+        bind.lifecycleOwner = viewLifecycleOwner
+        idPrep?.let { viewModelPrep.getPreparationItem(it) }
+    }
+
+    companion object{
+        const val AID_KIT_ID = "aid_id"
     }
 }
